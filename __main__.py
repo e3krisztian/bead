d36d2911860bd42c772a0f75baa087a15cd405cf
Krisztián Fekete <e3krisztian@gmail.com>
2025-08-18 23:40:00 +0200

    fix main script python version check

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 import sys
 
-assert sys.version_info >= (3, 7), "Python version 3.7 or newer is required"
+assert sys.version_info >= (3, 10), "Python version 3.10 or newer is required"
 
 if __name__ == "__main__":
     import bead_cli.main
